649d6b29fe5bd646987eb8ecb8626f61677482d6
Da-niel-G <da-niel-g.github.com>
2020-10-24 18:02:58 +0500

    add: function added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    str = n.toString();
+    res = str.split("").reverse().join("");
+    if (res.slice(-1) == '-') {
+        return Number(res.slice(0, -1))
+    }
+    else {
+        return res;
+    }
 }
